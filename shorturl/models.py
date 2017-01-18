00a384dfc032eba1305b57dca2e0d080797dc08c
WeiJie <454957891@qq.com>
2017-01-18 17:47:07 +0800

    change url to unique

--- shorturl/models.py
+++ shorturl/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 
 class ShortURL(models.Model):
-    url=models.CharField(max_length=220)
+    url=models.CharField(max_length=220,unique=True)
     shortcode=models.CharField(max_length=15,unique=True,blank=True)
     updated=models.DateTimeField(auto_now=True) #everytime the model is saved
     timestamp=models.DateTimeField(auto_now_add=True) #when model was created
